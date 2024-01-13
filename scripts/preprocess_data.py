529acfe1d0dfa78bb044ef5dec9681d3d802c1cb
Nguyen The Nam <namnguyen61031@gmail.com>
2024-01-13 12:26:33 +0100

    Add script to transform data

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+import sys
+sys.path.append('.')
+from src import melt_all_year_cols_into_one, process_fat_consumption_data
+
+# ============= Alcohol consumption data =============
+INPUT_FILE = "data/raw/wdi_tobaccoalcohol_population.csv"
+OUTPUT_FILE = "data/final/wdi_AlcoholConsumption.csv"
+
+alcohol_df = melt_all_year_cols_into_one(
+    input_file = INPUT_FILE, 
+    output_file = OUTPUT_FILE,
+    replace_dots = True,
+    remove_nan = True,
+    one_series = ("Series Name", "Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)"),
+    remove_cols = ["Series Code"])
+# =====================================================
+
+# ================ Fat consumption data ===============
+INPUT_FILE = "data/raw/daily_per_capita_fat_supply.csv"
+OUTPUT_FILE = "data/final/daily_per_capita_fat_supply_final.csv"
+
+fat_df = process_fat_consumption_data(INPUT_FILE, OUTPUT_FILE)
+# ======================================================
